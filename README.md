0143d4ba5d6a3874b42fb243ae063b10f922a99a
CodeSandbox <support@codesandbox.io>
2023-10-25 13:06:39 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,34 +1,42 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Flip card implementation using TailwindCSS and NextJS 
 
-## Getting Started
+## TailwindCSS Demo
+Open In TailwindCSS playground [https://play.tailwindcss.com/8ZWdqrpZvi](https://play.tailwindcss.com/8ZWdqrpZvi)
 
-First, run the development server:
+## NextJS Demo
+[]()
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
+## Setup Guide
+### Step 1: Add required custom utility classes
+This project use some css properties that are not yet in tailwindcss by default. However, you can add those custom utility classes in to your ```tailwind.conf.js``` file and use anywhere in the project. After adding those utility classes your ```tailwind.conf.js``` will look the one below. learn more about adding custom plugin in the [tailwind official documentation](https://tailwindcss.com/docs/plugins)
+
 ```
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin")
+export default {
+  theme: {
+    extend: {
+      // ...
+    },
+  },
+  plugins: [
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.rotate-y-180': {
+          transform: 'rotateY(180deg)'
+        },
+        '.preserve-3d': {
+          transformStyle: 'preserve-3d',
+        },
+        '.perspective': {
+          perspective: '1000px',
+        },
+        '.backspace-hidden': {
+          'backspaceVisibility': 'hidden'
+        }
+      })
+    })
+  ],
+}
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+```
--- components/FlipCard.tsx
+++ components/FlipCard.tsx
@@ -1,28 +1,79 @@
-import Image from "next/image"
+import Image from "next/image";
+import refactoringUI from "@/public/refactoringUI.png";
 
 export default function FlipCard() {
-    return(
-        <div className="perspective group bg-slate-700 rounded-lg">
-            <div className="relative w-60 h-80 overflow-hidden rounded-lg cursor-pointer transition-all delay-100 duration-700 preserve-3d group-hover:rotate-y-180">
-                <FlipCardFront />
-                <FlipCardBack />
-            </div>
-        </div>
-    )
+  return (
+    <div className="perspective group bg-slate-700 rounded-lg">
+      <div className="relative w-60 h-80 overflow-hidden rounded-lg cursor-pointer transition-all delay-100 duration-700 preserve-3d group-hover:rotate-y-180">
+        <FlipCardFront />
+        <FlipCardBack />
+      </div>
+    </div>
+  );
 }
 
 function FlipCardFront() {
-    return (
-        <div className="absolue inset-0  backspace-hidden preserve-3d transition-all duration-100 delay-300 group-hover:opacity-0 group-hover:-z-10 z-20 bg-blue-500">
-            {/* content */}
-            <Image 
-              src="https://www.refactoringui.com/_next/static/media/book.5380927448c9872170bbc9fc9e5828c4.png"
-              alt=""
-            />
-        </div>
-    )
+  return (
+    <div className="absolute inset-0  backspace-hidden preserve-3d transition-all duration-100 delay-300 group-hover:opacity-0 group-hover:-z-10 z-20 bg-blue-500">
+      {/* content */}
+      <Image src={refactoringUI} alt="" width={240} height={320} />
+    </div>
+  );
 }
 
 function FlipCardBack() {
-
+  return (
+    <div className="absolute inset-0 rotate-y-180 backspace-hidden transition-all z-10 bg-slate-800">
+      {/* content */}
+      <div className="py-2 px-3 flex flex-col items-center">
+        <h2 className="font-bold text-center text-white text-2xl">
+          Refactoring UI
+        </h2>
+        <h3 className="font-semibold text-lg text-slate-100 pt-2">Authors</h3>
+        <p className="text-xs text-slate-200 pt-0">
+          <span>Adam Wathan</span> <br />
+          <span>Steve Schoger</span>
+        </p>
+        <div className="pt-3">
+          <p className="text- italic text-center text-slate-400 pt-0">
+            Learn how to design beautiful user interfaces by yourself using
+            specific tactics explained from a developer's point-of-view.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 space-y-2">
+          <a
+            href="https://www.refactoringui.com"
+            className="font-bold text-sm text-white bg-slate-800 transition-transform duration-700 delay-200 hover:scale-x-125"
+          >
+            <span className="flex items-baseline space-x-1 justify-center">
+              <span>More info</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 15"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-3 h-3 stroke-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </span>
+          </a>
+          <a
+            href="https://www.refactoringui.com/#get-refactoring-ui"
+            className="w-full py-3 px-10 text-center font-bold text-sm text-slate-800 rounded-lg bg-white shadow-md transition-all hover:shadow-lg hover:scale-110 duration-700 delay-200 shadow-slate-500 hover:shadow-slate-300"
+            target="_blank"
+            rel="noopener"
+          >
+            Buy now
+          </a>
+        </div>
+      </div>
+      {/* End content */}
+    </div>
+  );
 }
